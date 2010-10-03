03a923c85d4d8c66799237443c06349d5f12f4a0
Cristian Planas <gawyntrakand@gmail.com>
2010-10-03 13:43:29 +0200

    Abans de pujar a la web

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ set :domain, 'fundidoanegro.net'
 set :application, 'fundidoanegro'
 
 # the rest should be good
-set :repository,  "#{gawyn}@#{fundidoanegro.net}:git/#{fundidoanegro}.git"
-set :deploy_to, "/home/#{gawyn}/#{fundidoanegro.net}"
+set :repository,  "gawyn@fundidoanegro.net:git/fundidoanegro.git"
+set :deploy_to, "/home/gawyn/fundidoanegro.net"
 set :deploy_via, :remote_cache
 set :scm, 'git'
 set :branch, 'master'
