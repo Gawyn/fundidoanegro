bf5de27b8eb98d1eda63fdf24d69d958bda3cc27
Cristian Planas <gawyntrakand@gmail.com>
2010-10-04 14:12:24 +0200

    A per Heroku

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.0.0'
 
 # Use unicorn as the web server
 # gem 'unicorn'
+gem 'sqlite3-ruby', :require => 'sqlite3'
 
 # Deploy with Capistrano
 gem 'capistrano'
--- db/migrate/20100927104313_addimages.rb
+++ db/migrate/20100927104313_addimages.rb
@@ -1,13 +1,13 @@
 class Addimages < ActiveRecord::Migration
   def self.up
-    add_column :criticas, :image_file_name, :string
-    add_column :criticas, :image_content_type, :string
-    add_column :criticas, :image_file_size, :integer
-    add_column :criticas, :image_updated_at, :datetime
     add_column :especials, :image_file_name, :string
     add_column :especials, :image_content_type, :string
     add_column :especials, :image_file_size, :integer
     add_column :especials, :image_updated_at, :datetime
+    add_column :criticas, :image_file_name, :string
+    add_column :criticas, :image_content_type, :string
+    add_column :criticas, :image_file_size, :integer
+    add_column :criticas, :image_updated_at, :datetime
     add_column :articulos, :image_file_name, :string
     add_column :articulos, :image_content_type, :string
     add_column :articulos, :image_file_size, :integer
