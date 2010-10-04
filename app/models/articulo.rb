765e42ffbe6032a3bb611dbaaf87ca1de7450405
Cristian Planas <gawyntrakand@gmail.com>
2010-10-04 16:29:52 +0200

    Arreglat error de les rutes de s3

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -2,7 +2,7 @@ class Articulo < ActiveRecord::Base
     has_attached_file :image,
 	 :storage => :s3,
 	 :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-	 :path => ":attachment/:id/:style.:extension"
+	 :path => ":attachment/articulo/:id/:style.:extension"
 	belongs_to :especial
 	belongs_to :user
 end
--- app/models/especial.rb
+++ app/models/especial.rb
@@ -2,7 +2,7 @@ class Especial < ActiveRecord::Base
     has_attached_file :image,
 	 :storage => :s3,
 	 :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-	 :path => ":attachment/:id/:style.:extension"
+	 :path => ":attachment/especial/:id/:style.:extension"
 	has_many :articulos
 	has_many :criticas
 end
