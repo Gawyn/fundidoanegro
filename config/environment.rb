8692784735d4d008675eccad0fcc5edc2319ec66
Cristian Planas <gawyntrakand@gmail.com>
2010-10-04 02:34:18 +0200

    Canviat l'ordre del environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,15 @@
 # Load the rails application
-require File.expand_path('../application', __FILE__)
-
 ENV['RAILS_ENV']= 'production'
-
-# Initialize the rails application
-Fundidoanegro::Application.initialize!
 
 if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
   ENV['GEM_PATH'] = '/home/USERNAME/.gems' + ':/usr/lib/ruby/gems/1.8'
 end
+
+require File.expand_path('../application', __FILE__)
+
+
+
+# Initialize the rails application
+Fundidoanegro::Application.initialize!
+
 
