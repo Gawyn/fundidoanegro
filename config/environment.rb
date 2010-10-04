3723baa41ae5dbef6995a6542c34eb926aed45d2
Cristian Planas <gawyntrakand@gmail.com>
2010-10-04 02:12:32 +0200

    Arreglat problema entre les gems i el passenger

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ ENV['RAILS_ENV']= 'production'
 
 # Initialize the rails application
 Fundidoanegro::Application.initialize!
+
+if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
+  ENV['GEM_PATH'] = '/home/gawyn/.gems' #+ ':/usr/lib/ruby/gems/1.8'  # Need this or Passenger fails to start
+end
+
